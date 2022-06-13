2257d6d4633732d641d7d78dad9cb195544733af
Gabriel Soares <gsoaresdesouza@live.com>
2022-06-12 23:47:53 -0300

    Database seeding

--- src/main/java/com/devsuperior/workshopmongo/models/entities/User.java
+++ src/main/java/com/devsuperior/workshopmongo/models/entities/User.java
@@ -9,21 +9,21 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "users")
 public class User {
-	
+
 	@Id
 	private String id;
 	private String name;
 	private String email;
-	
+
 	@DBRef(lazy = true)
-	public List<Post> posts = new ArrayList<>();	
-	
+	public List<Post> posts = new ArrayList<>();
+
 	public User() {
-		
+
 	}
 
 	public User(String id, String name, String email) {
-		
+
 		this.id = id;
 		this.name = name;
 		this.email = email;
@@ -53,5 +53,8 @@ public class User {
 		this.email = email;
 	}
 
+	public List<Post> getPosts() {
+		return posts;
+	}
 
 }
